19c688c39c0e6dd7f4369316b7c74473c8a9dbf8
lonely43 <iladenisov327@gmail.com>
2025-03-13 01:43:47 +0300

    add translate ability

--- src/components/blocks/Facts.vue
+++ src/components/blocks/Facts.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
-import { getFact } from "@/scripts/api"
+import { getFact, translate } from "@/scripts/api"
 import { onMounted, ref } from "vue"
 import ButtonComp from "../UI/ButtonComp.vue"
 
 const fact = ref<string>("Looking for fact...")
 
+const lang = ref<string>("ru")
+const langCodes = ref(["ru", "en", "zh"])
+
 onMounted(async () => {
-	getNewFact()
+	await getNewFact()
 })
 
 async function getNewFact() {
 	fact.value = "Looking for fact..."
 	fact.value = await getFact()
+}
+
+async function translateFact() {
+	fact.value = await translate(fact.value, lang.value)
 }
 </script>
 
@@ -28,12 +35,21 @@ async function getNewFact() {
 			</div>
 
 			<div class="btns">
-				<ButtonComp id="seeFact" @click="getNewFact" type="primary">
-					New fact
-				</ButtonComp>
-				<ButtonComp type="secondary">
-					<img src="/src/assets/imgs/globe.svg" alt="globe">
-				</ButtonComp>
+				<ButtonComp id="seeFact" @click="getNewFact" type="primary"> New fact </ButtonComp>
+
+				<div class="dropdown">
+					<div class="dropdown-title">
+						<ButtonComp type="secondary" @click="translateFact">
+							<img src="/src/assets/imgs/globe.svg" alt="globe" />
+						</ButtonComp>
+					</div>
+
+					<ul class="dropdown-list">
+						<li v-for="code in langCodes">
+							<p @click="lang = code">{{ code }}</p>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -56,7 +72,7 @@ async function getNewFact() {
 		min-height: 20rem;
 
 		padding: 4rem 2rem;
-		
+
 		position: relative;
 
 		display: flex;
@@ -66,7 +82,7 @@ async function getNewFact() {
 		gap: 2rem;
 
 		background-color: $color-background;
-		box-shadow: 0 0 1rem .2rem rgb(226, 226, 226);
+		box-shadow: 0 0 1rem 0.2rem rgb(226, 226, 226);
 		border-radius: 1rem;
 
 		.info {
@@ -106,6 +122,78 @@ async function getNewFact() {
 			justify-content: center;
 			align-items: center;
 			gap: 1rem;
+		}
+	}
+
+	.dropdown {
+		position: relative;
+
+		@keyframes showDrop {
+			0%{
+				display: flex;
+				opacity: 0;
+			}
+			100%{
+				display: flex;
+				opacity: 1;
+			}
+		}
+
+		&:hover .dropdown-list {
+			opacity: 1;
+			display: flex;
+		}
+
+		.dropdown-list {
+			animation: showDrop 400ms;
+			$dropdown-gap: .6rem;
+
+			position: relative;
+
+			&::before{
+				position: absolute;
+				left: 0;
+				bottom: 101%;
+				width: 100%;
+				height: $dropdown-gap;
+				content: "";
+			}
+
+			z-index: 1;
+
+			display: none;
+			transition: 400ms;
+			opacity: 0;
+
+			position: absolute;
+			top: calc(100% + $dropdown-gap);
+			left: 50%;
+			transform: translateX(-50%);
+
+			padding: 0.4rem 0.6rem;
+
+			background-color: $color-gray;
+			border: 0.1rem solid $color-gray-dark;
+			border-radius: 0.6rem;
+
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			gap: 0.2rem;
+
+			li {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+
+				p {
+					cursor: pointer;
+					transition: opacity 200ms;
+					&:hover {
+						opacity: 0.6;
+					}
+				}
+			}
 		}
 	}
 }
--- src/scripts/api.ts
+++ src/scripts/api.ts
@@ -1,3 +1,17 @@
 export async function getFact(): Promise<string> {
 	return (await fetch("https://catfact.ninja/fact").then((response) => response.json())).fact
 }
+
+export async function translate(sourceText: string, lang: string): Promise<string> {
+	let translatedText = ""
+	let url = new URL(`https://translate.googleapis.com/translate_a/single?client=gtx&dt=t`)
+	url.searchParams.set("q", sourceText) // source text
+	url.searchParams.set("sl", "auto") // source language
+	url.searchParams.set("tl", lang) // into language
+
+	await fetch(url)
+		.then((res) => res.json())
+		.then((data) => (translatedText = data[0][0][0]))
+
+	return translatedText
+}
